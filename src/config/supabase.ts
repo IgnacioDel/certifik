43f88295ad20d94867166b444856abaa9a965afe
IgnacioDel <157837390+IgnacioDel@users.noreply.github.com>
2024-02-04 13:25:06 -0600

    Update supabase.ts
    hardcoded la llave de supabase.ts en config para hacer pruebas...

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,11 +1,31 @@
+//import dotenv from "dotenv";
+//import { createClient } from "@supabase/supabase-js";
+
+//dotenv.config();
+
+//const supabase = createClient(
+//  process.env.SUPABASE_URL,
+//  process.env.SUPA_KEY
+//);
+
+//export default supabase;
+
 import dotenv from "dotenv";
 import { createClient } from "@supabase/supabase-js";
 
 dotenv.config();
 
+// const supabaseUrl = process.env.PUBLIC_SUPABASE_URL
+// const supabaseKey = process.env.SUPABASE_KEY
+// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVheGV5cWV3cmJxamJxYXFmbHJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg0NDI2NTcsImV4cCI6MjAxNDAxODY1N30.SAU-LAUY7DwHmNfxf1CNZSpwu0FAJxyzbRgoyEDZEKs"
+
+//export const supabase = createClient(supabaseUrl, supabaseKey)
+
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPA_KEY
+  "https://mnnbyrdnpuienzscjzjk.supabase.co",
+  supabaseKey
 );
 
 export default supabase;
